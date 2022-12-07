7c23c914c1fd4b20a27eb54415184e93192099c9
eudago <eudago@pop-os.localdomain>
2022-12-07 23:51:32 +0100

    init backend project with express

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+app.listen(port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
